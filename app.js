77ae6e86be7d930666e51d087b492c2c6fefd238
musa_abdullah <musaabdalwahed@gmail.com>
2020-10-20 16:43:49 +0200

    update

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const ejs = require('ejs');
 const path = require('path');
-const bodyParser = require("body-parser");
+
 const logger = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const fileUpload = require('express-fileupload')
+
 
 var app = express();
 
 // middlware config 
 
 app.use(logger('dev'));
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.set('view enging','ejs');
 app.set('viewe',path.join(__dirname,'/views'));
 app.use(express.static(path.join(__dirname,'/public')));
-app.use(fileUpload());
+
 
 const PORT = process.env.PORT || 3000;
 
